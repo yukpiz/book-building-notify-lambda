20d2e5a9085665d34f7a6094a4d852139a4ec990
yukpiz <yukpiz@gmail.com>
2020-02-10 11:25:03 +0900

    up

--- functions/notify/main.go
+++ functions/notify/main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/k0kubun/pp"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
 )
 
 const (
 	CRAWL_WEB_URL = "http://www.tokyoipo.com/ipo/schedule.php"
 )
+
+var (
+	debug = flag.Bool("debug", false, "run debug mode")
+)
+
+type IPOSchedule struct {
+	Index       int
+	CompanyName string
+}
 
 func Handler(ctx context.Context) error {
 	fmt.Println("START: book-building-notify-lambda")
@@ -19,9 +34,38 @@ func Handler(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+
+	var ss []*IPOSchedule
+
+	doc.Find(".h2_ipolist_name > a").Each(func(i int, s *goquery.Selection) {
+		ss = append(ss, &IPOSchedule{
+			Index:       i,
+			CompanyName: EUCJP2UTF8(s.Text()),
+		})
+	})
+
+	pp.Println(ss)
+
 	return nil
 }
 
+func EUCJP2UTF8(str string) string {
+	ret, err := ioutil.ReadAll(transform.NewReader(strings.NewReader(str), japanese.EUCJP.NewDecoder()))
+	if err != nil {
+		fmt.Println("WARNING: string encoding error")
+		fmt.Println(err)
+		return str
+	}
+	return string(ret)
+}
+
 func main() {
-	lambda.Start(Handler)
+	flag.Parse()
+	if *debug {
+		if err := Handler(context.Background()); err != nil {
+			panic(err)
+		}
+	} else {
+		lambda.Start(Handler)
+	}
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,7 @@ go 1.13
 require (
 	github.com/PuerkitoBio/goquery v1.5.1
 	github.com/aws/aws-lambda-go v1.13.3
+	github.com/k0kubun/pp v3.0.1+incompatible
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	golang.org/x/text v0.3.0
 )
